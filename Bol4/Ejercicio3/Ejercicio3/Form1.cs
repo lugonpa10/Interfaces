0bcaf1a388577fbe950635ce3b0a3629ef936b3f
lugonpa10 <lugonpa10@gmail.com>
2025-11-07 10:43:35 +0100

    changes

--- Bol4/Ejercicio3/Ejercicio3/Form1.cs
+++ Bol4/Ejercicio3/Ejercicio3/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,10 +25,30 @@ namespace Ejercicio3
 
         private void btnNuevaImg_Click(object sender, EventArgs e)
         {
+            string contenidoArchivo ="";
+            string rutaArchivo = "";
             using (OpenFileDialog ofd = new OpenFileDialog())
             {
+                ofd.Title = "Escoge una imagen";
+                ofd.InitialDirectory = "c:\\";
+                ofd.Filter = "Archivos JPG (*.jpg)|*.jpg|Archivos PNG (*.png)|*.png|Todos los archivos (*.*)|*.*;  ";
+                ofd.RestoreDirectory = true;
+
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    rutaArchivo=ofd.FileName;
+
+                    var fileStream = ofd.OpenFile();
+
+                    using (StreamReader sr = new StreamReader(fileStream))
+                    {
+                        contenidoArchivo=sr.ReadToEnd();
+                    }
+                    
+                }
 
             }
+            MessageBox.Show(contenidoArchivo, "File Content at path: " + rutaArchivo, MessageBoxButtons.OK);
         }
 
         
