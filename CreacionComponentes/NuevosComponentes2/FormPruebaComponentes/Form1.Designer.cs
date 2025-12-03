c1dd4b82749603568e68731efb3601b2e68d3cd5
lugonpa10 <lugonpa10@gmail.com>
2025-12-03 09:36:26 +0100

    changes

---
+++ CreacionComponentes/NuevosComponentes2/FormPruebaComponentes/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace FormPruebaComponentes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelTextBox1 = new NuevosComponentes2.LabelTextBox();
+            this.SuspendLayout();
+            // 
+            // labelTextBox1
+            // 
+            this.labelTextBox1.Location = new System.Drawing.Point(114, 74);
+            this.labelTextBox1.Name = "labelTextBox1";
+            this.labelTextBox1.Posicion = NuevosComponentes2.EPosicion.IZQUIERDA;
+            this.labelTextBox1.Separacion = 40;
+            this.labelTextBox1.Size = new System.Drawing.Size(310, 20);
+            this.labelTextBox1.TabIndex = 0;
+            this.labelTextBox1.TextLbl = "label1";
+            this.labelTextBox1.TextTxt = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private NuevosComponentes2.LabelTextBox labelTextBox1;
+    }
+}
+
