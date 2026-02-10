a105702cdc668206e2e0505c0a5223815793fb0a
lugonpa10 <lugonpa10@gmail.com>
2026-02-10 21:22:38 +0100

    changes

---
+++ BolCreacionComponentes/Ejercicio 5/Ejercicio 5/ValidateTextBox.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace Ejercicio_5
+{
+    public partial class ValidateTextBox : UserControl
+    {
+       public bool esValido = true;
+        public enum eTipo
+        {
+            Numerico, Textual
+        }
+
+        public ValidateTextBox()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            txt.Location = new Point(10, 10);
+
+            this.Height = txt.Height + 20;
+            txt.Width = this.Width - 20;
+
+
+            Color colorRecuadro;
+            Graphics g = e.Graphics;
+
+            if (esValido)
+            {
+                colorRecuadro = Color.Green;
+            }
+            else
+            {
+                colorRecuadro = Color.Red;
+            }
+
+
+
+            Pen p = new Pen(colorRecuadro);
+            g.DrawRectangle(p, 5, 5, this.Width -10,this.Height -10);
+
+
+
+
+        }
+
+        [Category("Mis propiedades")]
+        [Description("Accede al texto del componente")]
+        public string Texto
+        {
+            set
+            {
+                txt.Text = value;
+            }
+            get
+            {
+                return txt.Text;
+            }
+        }
+
+        [Category("Mis propiedades")]
+        [Description("Accede a la propiedad Multilinea del componente")]
+        public bool Multilinea
+        {
+            set
+            {
+                txt.Multiline = value;
+            }
+            get
+            {
+                return txt.Multiline;
+            }
+        }
+
+        private eTipo tipo = eTipo.Textual;
+        [Category("Mis propiedades")]
+        [Description("Define el tipo de dato del componente")]
+        public eTipo Tipo
+        {
+            set
+            {
+                tipo = value;
+            }
+            get
+            {
+                return tipo;
+            }
+        }
+
+        private void ValidarTexto()
+        {
+            esValido = true;
+            switch (tipo)
+            {
+
+                case eTipo.Numerico:
+
+
+                    string textoSinEspacios = txt.Text.Trim();
+                    foreach (Char c in textoSinEspacios)
+                    {
+                        if (!char.IsDigit(c))
+                        {
+                            esValido = false;
+                        }
+                    }
+                    break;
+                case eTipo.Textual:
+                    foreach (Char c in txt.Text)
+                    {
+                        if (!char.IsLetter(c) && c != ' ')
+                        {
+                            esValido = false;
+                        }
+                    }
+
+                    break;
+            }
+            Refresh();
+        }
+
+        [Category("Mis eventos")]
+        [Description("Accede al TextChange del textBox desde el nuevo componente ")]
+        public event EventHandler TextoCambiado;
+        protected virtual void OnTextoCambiado(EventArgs e)
+        {
+            TextoCambiado?.Invoke(this, e);
+        }
+
+
+        private void txt_TextChanged(object sender, EventArgs e)
+        {
+            ValidarTexto();
+            OnTextoCambiado(EventArgs.Empty);
+        }
+    }
+}
---
+++ BolCreacionComponentes/Ejercicio 5/ValidateTextBox/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace ValidateTextBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.validateTextBox1 = new Ejercicio_5.ValidateTextBox();
+            this.SuspendLayout();
+            // 
+            // validateTextBox1
+            // 
+            this.validateTextBox1.Location = new System.Drawing.Point(21, 12);
+            this.validateTextBox1.Multilinea = false;
+            this.validateTextBox1.Name = "validateTextBox1";
+            this.validateTextBox1.Size = new System.Drawing.Size(199, 119);
+            this.validateTextBox1.TabIndex = 0;
+            this.validateTextBox1.Texto = "";
+            this.validateTextBox1.Tipo = Ejercicio_5.ValidateTextBox.eTipo.Textual;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.validateTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Ejercicio_5.ValidateTextBox validateTextBox1;
+    }
+}
+
